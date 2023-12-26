81887c48cdf5257b4e2fbe3392956ab9b41d8753
imran <87297004+imranshaikh9930@users.noreply.github.com>
2023-12-26 16:17:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 //your JS code here. If required.
+
+const submit = document.querySelector("#submit");
+const isChecked = document.querySelector("#checkbox");
+const userName = document.querySelector("#username");
+const password = document.querySelector("#password");
+
+
+document.addEventListener("DOMContentLoaded",()=>{
+
+
+    const savedUser = localStorage.getItem("username");
+
+    if(savedUser){
+        const existingUser = document.createElement("button");
+        existingUser.id = "existing";
+        existingUser.textContent = 'Login as existing user';
+
+        document.body.appendChild(existingUser);
+
+        existingUser.addEventListener('click', function () {
+            alert('Logged in as ' + savedUser);
+          });
+    }
+})
+
+
+submit.addEventListener("click",(e)=>{
+
+    e.preventDefault();
+
+    if(isChecked.checked){
+        localStorage.setItem("username",JSON.stringify(userName.value));
+    }
+    alert('Logged in as ' + userName.value);
+   
+})
